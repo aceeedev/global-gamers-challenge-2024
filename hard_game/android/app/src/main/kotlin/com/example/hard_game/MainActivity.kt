0c5e9d6146dca83ee0fa5c53bd33d7216cee159d
aceeedev <cyberandrew03@gmail.com>
2024-01-21 21:00:08 -0800

    Initial flutter app

---
+++ hard_game/android/app/src/main/kotlin/com/example/hard_game/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hard_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ hard_game/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:hard_game/app.dart';
+
+void main() {
+  runApp(const App());
+}
